75dcc7c12e2e8e80de332d0230a3d77d336e15ed
Veyrack-IPPON <vlin@ippon.fr>
2021-04-21 17:02:34 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,334 @@
+// @flow
+
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import GraphiQL from "graphiql";
+import GraphiQLExplorer from "graphiql-explorer";
+import { /*buildClientSchema,*/ getIntrospectionQuery, parse } from "graphql";
+import { makeDefaultArg, getDefaultScalarArgValue } from "./CustomArgs";
+import "graphiql/graphiql.css";
+import "./App.css";
+import type { GraphQLSchema } from "graphql";
+// import * as fs from 'fs';
+import { Button, Modal, ModalHeader, ModalBody, Label, Col} from 'reactstrap';
+import { Control, LocalForm, Errors } from 'react-redux-form';
+var generatedScript = require('./script.js');
+
+var nomnoml = require('nomnoml');
+
+const required = (val) => val && val.length;
+const maxLength = (len) => (val) => !(val) || (val.length <= len);
+const minLength = (len) => (val) => val && (val.length >= len); 	
+
+// const { exec } = require('child_process');
+
+function fetcher(params: Object): Object {
+  return fetch(
+    "https://serve.onegraph.com/dynamic?app_id=c333eb5b-04b2-4709-9246-31e18db397e1",
+    {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(params)
+    }
+  )
+    .then(function(response) {
+      return response.text();
+    })
+    .then(function(responseBody) {
+      try {
+        return JSON.parse(responseBody);
+      } catch (e) {
+        return responseBody;
+      }
+    });
+}
+
+const DEFAULT_QUERY = `type User {
+  id: ID!
+  username: String
+  firstname: String
+  lastname: String
+  fullname: String
+  name: String @deprecated
+}
+
+type Stat {
+  id: ID!
+  views: Int
+  likes: Int
+  retweets: Int
+  responses: Int
+}
+
+type Notification {
+  id: ID!
+  type: String
+}
+
+type Meta {
+  id: ID!
+  count: Int
+}`;
+
+type State = {
+  schema: ?GraphQLSchema,
+  query: string,
+  explorerIsOpen: boolean
+};
+const handleGenerateScript = (jsonData,values) => {
+//   const fileData = JSON.stringify(jsonData);
+	var script = "#!/bin/bash\n\n" +
+		"schema=\"" + jsonData + "\"\n\n" +
+		"title=\"" + values.title + "\"\n\n" +
+		"framework=\"" + values.frontfw +"\"\n\n" +
+		"db=\"" + values.db + "\"\n" +
+    generatedScript.script
+
+  const blob = new Blob([script], {type: "application/sh"});
+//   console.log(blob)
+  const url = URL.createObjectURL(blob);
+  const link = document.createElement('a');
+  link.download = 'generatedSchema.sh';
+  link.href = url;
+  link.click();
+}
+
+class App extends Component<{}, State> {
+  _graphiql: GraphiQL;
+  state = { 
+    schema: null, 
+    query: DEFAULT_QUERY, 
+    explorerIsOpen: true ,
+  };
+
+
+  componentDidMount() {
+    fetcher({
+      query: getIntrospectionQuery()
+    }).then(result => {
+      const editor = this._graphiql.getQueryEditor();
+      editor.setOption("extraKeys", {
+        ...(editor.options.extraKeys || {}),
+        "Shift-Alt-LeftClick": this._handleInspectOperation
+      });
+
+      // this.setState({ schema: buildClientSchema(result.data) });
+    });
+  }
+
+  _handleInspectOperation = (
+    cm: any,
+    mousePos: { line: Number, ch: Number }
+  ) => {
+    const parsedQuery = parse(this.state.query || "");
+
+    if (!parsedQuery) {
+      console.error("Couldn't parse query document");
+      return null;
+    }
+
+    var token = cm.getTokenAt(mousePos);
+    var start = { line: mousePos.line, ch: token.start };
+    var end = { line: mousePos.line, ch: token.end };
+    var relevantMousePos = {
+      start: cm.indexFromPos(start),
+      end: cm.indexFromPos(end)
+    };
+
+    var position = relevantMousePos;
+
+    var def = parsedQuery.definitions.find(definition => {
+      if (!definition.loc) {
+        console.log("Missing location information for definition");
+        return false;
+      }
+
+      const { start, end } = definition.loc;
+      return start <= position.start && end >= position.end;
+    });
+
+    if (!def) {
+      console.error(
+        "Unable to find definition corresponding to mouse position"
+      );
+      return null;
+    }
+
+    var operationKind =
+      def.kind === "OperationDefinition"
+        ? def.operation
+        : def.kind === "FragmentDefinition"
+        ? "fragment"
+        : "unknown";
+
+    var operationName =
+      def.kind === "OperationDefinition" && !!def.name
+        ? def.name.value
+        : def.kind === "FragmentDefinition" && !!def.name
+        ? def.name.value
+        : "unknown";
+
+    var selector = `.graphiql-explorer-root #${operationKind}-${operationName}`;
+
+    var el = document.querySelector(selector);
+    el && el.scrollIntoView();
+  };
+
+	_handleEditQuery = (query: string): void => this.setState({ query });
+
+	_handleToggleExplorer = () => {
+		this.setState({ explorerIsOpen: !this.state.explorerIsOpen });
+	};
+
+	handleSubmit(values) {
+    this.toggleModal();
+    console.log('Current State is: ' + JSON.stringify(values));
+    alert('Current State is: ' + JSON.stringify(values));
+		handleGenerateScript(this.state.query,values)
+  }
+
+  toggleModal = () => {
+      this.setState({ isModalOpen: !this.state.isModalOpen});
+  };
+  _getQuery = (query: string): void => {
+    // console.log(query);
+    // handleGenerateScript(query["query"])
+    this.setState({query: query["query"]})
+    this.toggleModal()
+  }
+  _showNomnoml = (query : string): void => {
+    var src = '[nomnoml] is -> [awesome]';
+    // console.log(nomnoml.renderSvg(src));
+    var tag = nomnoml.renderSvg(src)
+    var div = document.getElementById("nom");
+    div.insertAdjacentHTML('beforeend',tag) 
+  }
+
+  render() {
+    const { query, schema } = this.state;
+    return (
+      <div className="graphiql-container">
+        <GraphiQLExplorer
+          schema={schema}
+          query={query}
+          onEdit={this._handleEditQuery}
+          onRunOperation={operationName =>
+            this._graphiql.handleRunQuery(operationName)
+          }
+          explorerIsOpen={this.state.explorerIsOpen}
+          onToggleExplorer={this._handleToggleExplorer}
+          getDefaultScalarArgValue={getDefaultScalarArgValue}
+          makeDefaultArg={makeDefaultArg}
+        />
+        <GraphiQL
+          ref={ref => (this._graphiql = ref)}
+          fetcher={fetcher}
+          schema={schema}
+          query={query}
+          onEditQuery={this._handleEditQuery}
+        >
+          <GraphiQL.Toolbar>
+            <GraphiQL.Button
+              onClick={() => this._graphiql.handlePrettifyQuery()}
+              label="Prettify"
+              title="Prettify Query (Shift-Ctrl-P)"
+            />
+            <GraphiQL.Button
+              onClick={() => this._graphiql.handleToggleHistory()}
+              label="History"
+              title="Show History"
+            />
+            <GraphiQL.Button
+              onClick={this._handleToggleExplorer}
+              label="Explorer"
+              title="Toggle Explorer"
+            />
+            <GraphiQL.Button
+              onClick={() => this._getQuery({query})}
+              label="Generate"
+              title="Generate script for Gamechanger"
+            />
+            <GraphiQL.Button
+              onClick={() => this._showNomnoml({query})}
+              label="Nomnoml"
+              title="Show Nomnoml"
+            />
+          </GraphiQL.Toolbar>
+        </GraphiQL>
+
+        <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+          <ModalHeader toggle={this.toggleModal}>Choix des paramètres</ModalHeader>
+          <ModalBody>
+            <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
+              <div className="form-group">
+                <Label htmlFor="title">Titre de l'application</Label>
+                <Col>
+                  <Control.text model=".title" id="title" name="title"
+                    placeholder="Titre" className="form-control" 
+                    validators={{required,minLength: minLength(2), maxLength: maxLength(15)}}/>
+                  <Errors
+                    className="text-danger"
+                    model=".title"
+                    show="touched"
+                    messages={{
+                        required: 'Nécessaire ',
+                        minLength: 'Doit être plus long que 2 caractères',
+                        maxLength: 'Doit être moins long que 15 caractères'
+                    }}
+                  />
+                </Col>
+              </div>
+              <div className="form-group">
+                <Label htmlFor="frontfw">Front</Label>
+                <Col>
+                  <Control.select model=".frontfw" id="frontfw" name="frontfw"
+                    className="form-control" validators={{required}}>>
+                    <option></option>
+                    <option>Amber</option>
+                    <option>Angular</option>
+                    <option>React</option>
+                    <option>Vue</option>
+                  </Control.select>
+                  <Errors
+                    className="text-danger"
+                    model=".frontfw"
+                    show="touched"
+                    messages={{
+                      required: 'Nécessaire',
+                    }}
+                  />
+                </Col>
+              </div>
+              <div className="form-group">
+                <Label htmlFor="db">Jeu de données inclus</Label>
+                <Col>
+                  <Control.select model=".db" id="db" name="db"
+                    className="form-control" validators={{required}}>
+                    <option></option>
+                    <option>Oui</option>
+                    <option>Non</option>
+                  </Control.select>
+                  <Errors
+                    className="text-danger"
+                    model=".db"
+                    show="touched"
+                    messages={{
+                      required: 'Nécessaire',
+                    }}
+                    />
+                </Col>
+              </div>
+              <Button type="submit" value="submit" color="primary">Generate</Button>
+
+            </LocalForm>
+          </ModalBody>
+        </Modal>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/script.js
@@ -0,0 +1,154 @@
+export const script = `
+echo "==> Generating graphql schema..."
+echo "$schema" > schema.graphql
+
+echo "==> Generating $title server using yeoman..."
+yes "" | yo aws-server-gamechanger $title schema.graphql
+
+cd $title/terraform
+#On suppose que l'utilisateur a fait un aws configure avec ses ID + Access Key + region eu-west-1 + json
+echo "==> Initializing terraform..."
+terraform init
+echo "==> Generating aws ressource..."
+yes "yes" | terraform apply -var-file="terraform.tfvar"
+
+# Getting poolID and clientID from cognito.txt
+echo "==> Getting cognito IDs..."
+cognitoValues=$(awk -F : '{print  $2}' cognito.txt)
+# Transform as array
+cognito=($cognitoValues)
+
+poolId=\${cognito[0]}
+echo "poolId : "$poolId
+clientId=\${cognito[1]}
+echo "clientId : "$clientId
+
+echo "==> Getting token from cognito..."
+cognitoIdp=$(aws cognito-idp admin-initiate-auth --user-pool-id $poolId --client-id $clientId --auth-flow ADMIN_NO_SRP_AUTH --auth-parameters USERNAME=admin@admin.fr,PASSWORD=password)
+cognitoTmp=($cognitoIdp)
+for i in "\${!cognitoTmp[@]}"; do 
+  if [ "\${cognitoTmp[$i]}" = '"IdToken":' ]
+  then
+    idToken=$(echo \${cognitoTmp[i+1]} | cut -d'"' -f2)
+  fi
+done
+
+echo "Cognito token : "$idToken
+
+echo "==> Getting endpoint url..."
+while IFS= read -r line; do url=$line; done < url.txt
+
+echo "endpoint url : "$url
+
+curl -X POST -H "Authorization: $idToken" -H "Content-Type: application/json" -d '{"initTable": "init"}' "$url"
+if [ "$db" = "Oui" ]
+then
+  echo "==> Adding fakes data..."
+  curl -X POST -H "Authorization: $idToken" -H "Content-Type: application/json" -d '{"fillTable": "fill"}' "$url"
+fi
+
+cd ../..
+
+echo "****************************"
+echo "******** FRONT PART ********"
+echo "****************************"
+
+case $framework in
+
+  "React")
+    rtitle=$title-react
+    echo "==> Generating React app $rtitle..."
+    npx create-react-app $rtitle
+    cd $rtitle
+    echo "==> Generating React front with yeoman..."
+    yo react-client-gamechanger ../schema.graphql
+    npm install
+    APIurl="herokuPrefix + '$url'"
+    echo "==> Putting api url '$APIurl' in src/constant/index.js..."
+    sed -i "s|herokuPrefix + ''|$APIurl|g" src/constants/index.js
+
+
+    echo "==> Putting values for aws in src/config/app-config.json..."
+    frontValues=$(awk -F : '{print  $2}' ../$title/terraform/front.txt)
+    echo $frontValues
+    # Transform as array
+    front=($frontValues)
+    userPoolId=\${front[0]}
+    clientId=\${front[1]}
+    echo "User pool id : "$userPoolId "\nClient id :"$clientId
+    sed -i "s/\"userPool\": \"\"/\"userPool\": \"$userPoolId\"/g" src/config/app-config.json
+    sed -i "s/\"userPoolBaseUri\": \"\"/\"userPoolBaseUri\": \"https:\/\/\${front[2]}.auth.eu-west-1.amazoncognito.com\"/g" src/config/app-config.json
+    sed -i "s/\"clientId\": \"\"/\"clientId\": \"$clientId\"/g" src/config/app-config.json
+    # sed -i "s/\"callbackUri\": \"\"/\"callbackUri\": \"http:\/\/localhost:3000\/callback\"/g" src/config/app-config.json
+    # sed -i "s/\"signoutUri\": \"\"/\"signoutUri\": \"http:\/\/localhost:3000\"/g" src/config/app-config.json
+
+    echo "==> Applying terraform..."
+    cd terraform
+    terraform init
+    yes "yes" | terraform apply
+
+    echo "==> Getting url from ids.txt..."
+    idsValues=$(awk -F : '{print $1 $2}' ids.txt)
+    # Transform as array
+    ids=($idsValues)
+    for i in "\${!ids[@]}"; do 
+        if [ "\${ids[$i]}" = 'URL_PRODUCTION' ]
+        then
+        ProductionUrl=https://\${ids[$i+1]}/callback
+        LogoutUrl=https://\${ids[$i+1]}
+        fi
+        if [ "\${ids[$i]}" = 'URL_STAGING' ]
+        then
+        StagingUrl=https://\${ids[$i+1]}/callback
+        # LogoutUrl=https://\${ids[$i+1]}
+        fi
+    done
+
+    echo "==> Updating callback : '$ProductionUrl' and logout url : '$LogoutUrl'..."
+    # echo "aws cognito-idp update-user-pool-client --user-pool-id $userPoolId --client-id $clientId --callback-urls $ProductionUrl --logout-urls $LogoutUrl --region eu-west-1"
+    aws cognito-idp update-user-pool-client --user-pool-id $userPoolId --client-id $clientId --callback-urls $ProductionUrl --logout-urls $LogoutUrl --region eu-west-1 --supported-identity-providers "COGNITO" --allowed-o-auth-flows "code" "implicit" --allowed-o-auth-scopes "phone" "email" "openid" "profile" "aws.cognito.signin.user.admin" --allowed-o-auth-flows-user-pool-client
+
+    sed -i "s|\"callbackUri\": \"\"|\"callbackUri\": \"$ProductionUrl\"|g" ../src/config/app-config.json
+    sed -i "s|\"signoutUri\": \"\"|\"signoutUri\": \"$LogoutUrl\"|g" ../src/config/app-config.json
+
+    echo "==> Getting aws credentials..."
+    awsCredentials=$(awk '{print $1" "$3}' ~/.aws/credentials)
+    credentials=($awsCredentials)
+    for i in "\${!credentials[@]}"; do 
+        if [ "\${credentials[$i]}" = 'aws_access_key_id' ]
+        then
+        echo "Key id : "\${credentials[$i+1]}
+        keyId=\${credentials[$i+1]}
+        fi
+        if [ "\${credentials[$i]}" = 'aws_secret_access_key' ]
+        then
+        echo "Access key : "\${credentials[$i+1]}
+        accessKey=\${credentials[$i+1]}
+        fi
+    done
+
+    echo "==> Putting aws access infos in deploy.js..."
+    sed -i "s/accessKeyId: ''/accessKeyId: '$keyId'/g" ../deploy.js
+    sed -i "s/secretAccessKey: ''/secretAccessKey: '$accessKey'/g" ../deploy.js
+
+    echo "==> Building app..."
+    cd ..
+    npm run build
+
+    echo "==> Deploiement en production..."
+    react-deploy deploy production
+
+    echo "Finished, available at $LogoutUrl"
+    ;;
+  "Ember")
+    echo "Generating Ember front..."
+    ;;
+  "Vue")
+    echo "Generating Vue front..."
+    ;;
+
+  *)
+    echo "ERROR no framework"
+    ;;
+esac
+`
