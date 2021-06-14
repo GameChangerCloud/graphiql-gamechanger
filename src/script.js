1c7e2557dcb5b1f676c393a3ede4d1c0ccdd5ff8
Bertrand PINEL <bpinel@ippon.fr>
2021-06-14 11:32:04 +0200

    add more delay in script for curl (90 versus 60) and change icon

--- src/script.js
+++ src/script.js
@@ -108,22 +108,22 @@ while IFS= read -r line; do url=$line; done < url.txt
 
 echo "endpoint url : "$url
 
-response=$(curl -m 60 -X POST -H "Authorization: $idToken" -H "Content-Type: application/json" -d '{"initTable": "init"}' "$url")
+response=$(curl -m 90 -X POST -H "Authorization: $idToken" -H "Content-Type: application/json" -d '{"initTable": "init"}' "$url")
 echo "$response"
 if [ "$response" != '{"statusCode":200,"body":"\\"Init done\\""}' ]
 then
-  echo "Failed... Stoping generation..."
+  echo "Failed... Stopping initTable generation..."
   exit 1
 fi
 
 if [ "$db" = "Oui" ]
 then
   echo "==> Adding fakes data..."
-  response=$(curl -m 60 -X POST -H "Authorization: $idToken" -H "Content-Type: application/json" -d '{"fillTable": "fill"}' "$url")
+  response=$(curl -m 90 -X POST -H "Authorization: $idToken" -H "Content-Type: application/json" -d '{"fillTable": "fill"}' "$url")
   echo "$response"
   if [ "$response" != '{"statusCode":200,"body":"\\"Fill done\\""}' ]
   then 
-    echo "Stoping generation..."
+    echo "Stopping fillTable generation..."
     exit 1
   fi
 fi
